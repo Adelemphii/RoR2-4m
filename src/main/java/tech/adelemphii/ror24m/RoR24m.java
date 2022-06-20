202e79c7b62324ec6a8cd7dc6094a3508a1fecfa
Adelemphii <demilitioowner@gmail.com>
2022-06-20 12:06:42 -0500

    initial setup

---
+++ src/main/java/tech/adelemphii/ror24m/RoR24m.java
@@ -0,0 +1,15 @@
+package tech.adelemphii.ror24m;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoR24m extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
